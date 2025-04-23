d6d3c9234d596113ae866125d1318dac4c715c06
Ramseys-kitchen <ramseyramatapa@gmail.com>
2025-04-23 15:41:47 +0200

    Debug Log
    Debugged

--- My project/Assets/Scripts/Test.cs
+++ My project/Assets/Scripts/Test.cs
@@ -5,6 +5,8 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        int number = 5000;
+        Debug.Log(number);
         
     }
 
